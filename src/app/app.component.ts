a25cfa200c0bf05bd9826a05a8c10edc03b69330
Akash Thengre <akash.thengre@gmail.com>
2018-05-20 15:26:54 +0530

    cleaning master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  myvalue1:number;
-  myvalue2:number;
-  istrue:boolean = false;
-  result;
-  
-  
-add(){
-this.result=(+this.myvalue1) + (+this.myvalue2);
-
-  }
-
-  checkresult(){    
-if(this.result != undefined)
-  return true;
-
-else
-return false;
-  }
-
 
 }
