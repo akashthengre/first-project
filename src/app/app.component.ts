ad3a5ed5b68a72f863d97058f5b4dc9be218aa84
Akash Thengre <akash.thengre@gmail.com>
2018-05-26 14:57:25 +0530

    bulp application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +9,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  name:string='Akash';
+  age:number=5;
+  myArray=[1,2,3];
+  myObj={
+    "name":"xyz",
+    "age":"1"
+  }
+  weekdays=["monday","tuesday","wednesday","thu","fri"];
+
+  bulbs=[0,0,0,0,0];
+  status1:boolean=true;
+  x=['../assets/bulb-off.png','../assets/bulb-off.png','../assets/bulb-off.png','../assets/bulb-off.png','../assets/bulb-off.png'];
+  y=['Off','Off','Off','Off','Off'];
+  ind;
+  switch(ind)
+{
+  if(this.status1==false){
+ this.status1=true;
+this.x[ind]='../assets/bulb-on.png';
+this.y[ind]='OFF';
+}
+else  {
+  this.status1=false;  
+  this.x[ind]='../assets/bulb-off.png';
+  this.y[ind]='On';
+} 
+}
+
+
+
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+// for path
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
+import { MysecondComponent } from './mysecond/mysecond.component';
+
+// angular
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent , 
+    MyfirstcomponentComponent, MysecondComponent
   ],
   imports: [
     BrowserModule,FormsModule
