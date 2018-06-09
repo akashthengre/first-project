7d58d34fb2a72ca2ea4c6c84475ba6958255fd37
Akash Thengre <akash.thengre@gmail.com>
2018-06-09 11:16:41 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component,
+    C2Component,
+    C3Component
   ],
   imports: [
     BrowserModule,FormsModule
