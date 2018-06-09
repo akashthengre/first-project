a6656e382d9ade7487a7643acbe3208dce3f7e2f
Akash Thengre <akash.thengre@gmail.com>
2018-06-09 12:04:45 +0530

    routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes  } from '@angular/router';//step1
+import { C1Component } from '../c1/c1.component';
+import { C3Component } from '../c3/c3.component';
+import { C2Component } from '../c2/c2.component';
+import { DefaultComponent } from '../default/default.component';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
+
+//step2
+const routes : Routes = [
+  {path:'c1',component:C1Component},
+  {path:'c2',component:C2Component},
+  {path:'c3',component:C3Component},
+  {path:'',component:DefaultComponent},
+  {path:'**',component:PagenotfoundComponent} 
+];
+
+@NgModule({
+  imports: [
+  RouterModule.forRoot(routes)  //step3
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 import { C3Component } from './c3/c3.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DefaultComponent } from './default/default.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     C1Component,
     C2Component,
-    C3Component
+    C3Component,
+    DefaultComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
