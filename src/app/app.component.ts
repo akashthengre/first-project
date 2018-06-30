b276b17a008851bdb40e0f349674da6d976a0f76
Akash Thengre <akash.thengre@gmail.com>
2018-06-30 11:01:50 +0530

    ng content example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  // ngOnInit(){
+  //   alert('in onit');
+  //   }
 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,FormsModule
