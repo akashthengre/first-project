9bf1fe25e7398392ba0b1b45677b8784c806dbf6
Akash Thengre <akash.thengre@gmail.com>
2018-07-06 10:41:09 +0530

    children routing assignment

--- src/app/home/homerouting.module.ts
+++ src/app/home/homerouting.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { HomeFooterComponent } from './home-footer/home-footer.component';
 import {RouterModule,Routes} from '@angular/router';
+import { HomeComponent } from './home.component';
 
 
 const routes : Routes = [
+  {path : 'home' , component : HomeComponent},
   {path:'home',
   children:[
     {path:'home-header',component:HomeHeaderComponent},
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{Routes,RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes:Routes=[];
+const routes:Routes=[
+  {path : 'home' , component : HomeComponent},
+  {path : 'about' , component : AboutusComponent},
+  {path : 'contact' , component : ContactComponent},
+  {path : '' , component : HomeComponent},
+];
 
 @NgModule({
   imports: [
