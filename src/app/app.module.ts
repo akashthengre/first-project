e1ed748bc6cd88f11a32b952233f2439d75e1560
Akash Thengre <akash.thengre@gmail.com>
2018-07-22 15:56:44 +0530

    custom directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,FormsModule
